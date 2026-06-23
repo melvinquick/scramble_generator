c174fdb993c478bb6a6c9a3b6677a7ac5c8bdba3
melvinquick <melvinquick@proton.me>
2026-06-23 14:17:21 -0400

    Version bump to update dependencies.

--- install.py
+++ install.py
@@ -101,7 +101,7 @@ def install():
     venv_path = get_venv_path()
     create_venv(venv_path)
     install_app(venv_path)
-    version = "1.20.4"
+    version = "1.20.5"
     icon = get_icon(venv_path)
     python = get_python_path(venv_path)
     app = get_app_path(venv_path)
